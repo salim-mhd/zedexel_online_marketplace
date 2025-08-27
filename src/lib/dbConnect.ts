07a57831256f664fa20ab7adcae27c5f0ba8eca3
muahmmed salim <salimmhd1280@gmail.com>
2025-08-27 23:23:47 +0530

    DB connected issue fixed

--- src/lib/dbConnect.ts
+++ src/lib/dbConnect.ts
@@ -1,7 +1,6 @@
 import mongoose from "mongoose";
 
-const MONGODB_URI =
-  process.env.MONGODB_URI || "mongodb://localhost:27017/zedexel";
+const MONGODB_URI = process.env.MONGODB_URI as string;
 
 if (!MONGODB_URI) {
   throw new Error("Please add MONGODB_URI in .env.local");
